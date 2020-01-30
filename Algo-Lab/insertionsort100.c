5a831a325788696d89505254301c3f7d38589af1
Pranav Shekhar <pranavshekhar1309@gmail.com>
2020-01-30 20:16:02 +0530

    Update and rename insertionsort100.cpp to insertionsort100.c

---
+++ Algo-Lab/insertionsort100.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void selectionSort(int*);
+int *getRandom();
+
+void selectionSort(int *arr)
+{
+    int n = 100, min, temp;
+    for (int i = 0; i < n - 1; i++)
+    {
+
+        min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    printf("The Sorted Generated Sequence = \n");
+    
+
+    for(int i=0;i<n;i++){
+        printf("%d",arr[i]);
+        printf("\n");
+    }
+
+}
+
+
+
+int *getRandom()
+{
+
+    int *temp = (int *)malloc(100*sizeof(int));
+    for (int i = 1; i <= 100; i++)
+    {
+        temp[i] = rand() % 100; //get random number
+    }
+
+    return temp;
+}
+
+
+
+int main()
+{
+    clock_t start, end;
+
+
+    int *a1 = getRandom();
+
+
+    start = clock();
+
+     selectionSort(a1);
+
+    end = clock();
+
+    double elapsed = (double)(end - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("Time elapsed in ms: %f", elapsed);
+
+    return 0;
+}
